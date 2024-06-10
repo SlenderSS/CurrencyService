d3279b545ab84824c688bea83a9ca9fc513d1582
Oleksandr Olkhovyk <oleksandr.olkhovyk.ki.2020@lpnu.ua>
2024-06-11 00:23:43 +0300

    Created currency model

---
+++ CurrencyService/Models/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyService.Models
+{
+    public class Currency
+    {
+        public int r030 { get; set; }
+        public string txt { get; set; }
+        public decimal rate { get; set; }
+        public int cc { get; set; }
+        public DateTime exchangedate { get; set; }
+    }
+}

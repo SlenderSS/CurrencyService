12d759712e674cc91a12ed638ca2040664fe3bf8
Oleksandr Olkhovyk <oleksandr.olkhovyk.ki.2020@lpnu.ua>
2024-06-13 17:44:04 +0300

    Changed solution`s name, added logging and refactored code

---
+++ CurrencyService/Service.cs
@@ -0,0 +1,175 @@
+﻿using CsvHelper;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CurrencyService
+{
+    public class Service
+    {
+        private string _directoryPath;
+        private readonly ILogger<Service> _logger;
+        private readonly HttpClient _httpClient;
+        public Service(ILogger<Service> logger, string path)
+        {
+
+            _logger = logger;
+            _httpClient = new HttpClient();
+            _directoryPath = path;
+
+        }
+
+        public async Task Run(string baseUrl, string fileName, string format, int interval)
+        {
+            CheckDirectory(_directoryPath);
+
+            DateTime date;
+            string directoryPathNow = string.Empty;
+
+            await Task.Run(async () =>
+            {
+                while (true)
+                {
+                    date = DateTime.Now;
+
+                    if (string.IsNullOrWhiteSpace(directoryPathNow) || !directoryPathNow.Contains(date.ToString("dd_MM_yyyy")))
+                        directoryPathNow = Path.Combine(_directoryPath, $"{fileName}_{date.ToString("dd_MM_yyyy")}");
+
+                    CheckDirectory(directoryPathNow);
+
+                    var currencyRate = await GetCurrencies(baseUrl, "json");
+
+                    WriteToFile(
+                        currencyRate,
+                    directoryPathNow,
+                        string.Join("_", fileName, date.ToString("dd_MM_yyyy_HH-mm")),
+                        format);
+
+                    await Task.Delay(interval);
+                }
+            });
+        }
+
+        private async Task<string> GetCurrencies(string baseUrl, string format)
+        {
+            try
+            {
+                var url = baseUrl + (format == "csv" ? "json" : format);
+                HttpResponseMessage message = await _httpClient.GetAsync(url);
+                message.EnsureSuccessStatusCode();
+
+                string responseBody = await message.Content.ReadAsStringAsync();
+
+                _logger.LogInformation($"Received currency rate");
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                _logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+
+        private void CheckDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+                _logger.LogInformation($"Created directory: {directoryPath}");
+            }
+        }
+
+        public void WriteToFile(string jsonContent, string directory, string fileName, string format)
+        {
+            CheckDirectory(directory);
+            var file = fileName + "." + format;
+            if (format == "csv")
+                WriteToCsvFile(jsonContent, directory, file, directory);
+            else
+            {
+
+                var path = Path.Combine(directory, file);
+                try
+                {
+                    using (var writer = new StreamWriter(path, false))
+                    {
+                        writer.Write(jsonContent);
+                        _logger.LogInformation($"The exchange rate has been successfully saved to the file {file}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.Message);
+                    throw;
+                }
+
+
+            }
+
+        }
+
+        public void WriteToCsvFile(string jsonContent, string directory, string file, string format)
+        {
+            CheckDirectory(directory);
+
+            var dt = JsonConvert.DeserializeObject<DataTable>(jsonContent);
+            try
+            {
+
+                using (var writer = new StreamWriter(Path.Combine(directory, file), false, Encoding.UTF8))
+                {
+                    using (var csv = new CsvWriter(writer, CultureInfo.CurrentCulture))
+                    {
+
+                        foreach (DataColumn column in dt.Columns)
+                        {
+                            csv.WriteField(column.ColumnName);
+                        }
+                        csv.NextRecord();
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            for (var i = 0; i < dt.Columns.Count; i++)
+                            {
+                                csv.WriteField(row[i]);
+                            }
+                            csv.NextRecord();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+
+            }
+
+
+        }
+        private static void GetConfiguration(out string format,
+                                      out string fileName,
+                                      out string baseUrl,
+                                      out string directoryName,
+                                      out string directoryPath,
+                                      out TimeSpan interval)
+        {
+            format = "json";
+            fileName = "curRate";
+            directoryName = "CurrencyRates";
+            directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), directoryName);
+            baseUrl = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?";
+            interval = new TimeSpan(61000);
+        }
+
+    }
+
+}
--- CurrencyService/Service1.cs
+++ CurrencyService/Service1.cs
@@ -1,20 +1,11 @@
-﻿using CsvHelper;
-using Microsoft.SqlServer.Server;
-using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+
 
 
 namespace CurrencyService
@@ -31,136 +22,184 @@ namespace CurrencyService
 
 
 
-        protected override void OnStart(string[] args)
+        protected override async void OnStart(string[] args)
         {
             Debugger.Launch();
-
 
             GetConfiguration(out string format,
                              out string fileName,
                              out string baseUrl,
                              out string directoryName,
-                             out TimeSpan interval);
+                             out string directoryPath,
+                             out int interval);
 
-            var directoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, directoryName);
 
-            if (!Directory.Exists(directoryPath))
-                Directory.CreateDirectory(directoryPath);
-
-            
-
-  
-            Task.Run(async () =>
+            ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
             {
-                if (!Directory.Exists(directoryPath))
-                    Directory.CreateDirectory(directoryPath);
-
-                //using (var writer = new StreamWriter(
-                //    Path.Combine(directoryCreate, fileName + "." + format), false, Encoding.UTF8))
-                //{
-                //    writer.Write(baseUrl + format);
-                //}
-                DateTime date;
-                using (HttpClient client = new HttpClient())
-                {
-                    while (!_canceled)
-                    {
-                        var body = await GetCurrencies(client, baseUrl, "json");
-
-                        date = DateTime.Now.Date;
-                        if (!Directory.Exists(Path.Combine(directoryPath, $"{fileName}_{date.ToString("dd_MM_yyyy")}")))
-
-                        await Task.Delay(interval);
-                    }
-                }
+                LoggerConfiguration loggerConfiguration = new LoggerConfiguration()
+                .WriteTo.File(Path.Combine("", "logs.txt"), rollingInterval: RollingInterval.Day);
+                builder.AddSerilog(loggerConfiguration.CreateLogger());
             });
-        }
 
-        private async Task<string> GetCurrencies(HttpClient httpClient, string baseUrl, string format)
-        {
-            try
-            {
-                var url = baseUrl + format == "csv" ? "json" : format;
-                HttpResponseMessage message = await httpClient.GetAsync(url);
-                message.EnsureSuccessStatusCode();
+            Service service = new Service(
+                loggerFactory.CreateLogger<Service>(),
+                @"C:\Users\Alex\Desktop\ExchangeRates"
+                );
 
-                string responseBody = await message.Content.ReadAsStringAsync();
-
-                return responseBody;
-            }
-            catch (HttpRequestException e)
-            {
-
-                Debug.WriteLine(e.Message);
-                return null;
-            }
-
-
+            await service.Run(baseUrl, fileName, format, interval);
         }
 
 
-        public void WriteToFile(string jsonContent, string path, string fileName, string format)
-        {
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            if (format == "csv")
-                WriteToCsvFile(jsonContent, path, fileName, path);
-            else
-            {
-                using (var writer = new StreamWriter(Path.Combine(path, fileName + "." + format), false))
-                {
-                    writer.Write(jsonContent);
-                }
-            }
-
-        }
-
-        public void WriteToCsvFile(string jsonContent, string path, string fileName, string format)
-        {
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            var dt = JsonConvert.DeserializeObject<DataTable>(jsonContent);
-
-            using (var writer = new StreamWriter(Path.Combine(path, fileName + "." + format), false, Encoding.UTF8))
-            {
-                using (var csv = new CsvWriter(writer, CultureInfo.CurrentCulture))
-                {
-
-                    foreach (DataColumn column in dt.Columns)
-                    {
-                        csv.WriteField(column.ColumnName);
-                    }
-                    csv.NextRecord();
-
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        for (var i = 0; i < dt.Columns.Count; i++)
-                        {
-                            csv.WriteField(row[i]);
-                        }
-                        csv.NextRecord();
-                    }
-                }
-            }
-        }
         private void GetConfiguration(out string format,
                                       out string fileName,
                                       out string baseUrl,
                                       out string directoryName,
-                                      out TimeSpan interval)
+                                      out string directoryPath,
+                                      out int interval)
         {
+
             format = ConfigurationManager.AppSettings["format"];
             fileName = ConfigurationManager.AppSettings["filename"];
-            directoryName = ConfigurationManager.AppSettings["dirname"];
-            baseUrl = ConfigurationManager.AppSettings["url"];
-            interval = new TimeSpan(Convert.ToInt64(ConfigurationManager.AppSettings["interval"]));
+            directoryPath = ConfigurationManager.AppSettings["dirpath"];
+            directoryName = Path.GetDirectoryName(directoryPath);
+            baseUrl = ConfigurationManager.AppSettings["baseurl"];
+            interval = int.Parse(ConfigurationManager.AppSettings["format"]);
         }
+
+
+        //private async Task<string> GetCurrencies(HttpClient httpClient, string baseUrl, string format)
+        //{
+        //GetConfiguration(out string format,
+        //                 out string fileName,
+        //                 out string baseUrl,
+        //                 out string directoryName,
+        //                 out TimeSpan interval);
+
+        //Task.Run(async () =>
+        //{
+        //    var baseDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, directoryName);
+
+        //    CheckDirectory(baseDirectory, string.Empty);
+
+        //    DateTime date;
+        //    string directoryPathNow = string.Empty;
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        while (!_canceled)
+        //        {
+        //            date = DateTime.Now.Date;
+
+        //            if (string.IsNullOrWhiteSpace(directoryPathNow) || !directoryPathNow.Contains(date.ToString("dd_MM_yyyy")))
+        //                directoryPathNow = Path.Combine(baseDirectory, $"{fileName}_{date.ToString("dd_MM_yyyy")}");
+
+        //            CheckDirectory(directoryPathNow, baseDirectory);
+
+        //            var currencyRate = await GetCurrencies(client, baseUrl, "json");
+
+        //            WriteToFile(
+        //                currencyRate, 
+        //                directoryPathNow,
+        //                baseDirectory, 
+        //                string.Join("_",fileName, date.ToString("dd_MM_yyyy_HH:mm")), 
+        //                format);
+
+        //            await Task.Delay(interval);
+        //        }
+        //    }
+        //});
+
+
+
+        //    try
+        //    {
+        //        var url = baseUrl + format == "csv" ? "json" : format;
+        //        HttpResponseMessage message = await httpClient.GetAsync(url);
+        //        message.EnsureSuccessStatusCode();
+
+        //        string responseBody = await message.Content.ReadAsStringAsync();
+
+        //        return responseBody;
+        //    }
+        //    catch (HttpRequestException e)
+        //    {
+
+        //        Debug.WriteLine(e.Message);
+        //        return null;
+        //    }
+
+
+        //}
+
+
+        //private void CheckDirectory(string directoryPath, string baseDirectory)
+        //{
+        //    if (!Directory.Exists(directoryPath))
+        //    {
+        //        Directory.CreateDirectory(directoryPath);
+        //    }
+        //}
+
+        //public void WriteToFile(string jsonContent, string directory, string baseDirectory, string fileName, string format)
+        //{
+        //    CheckDirectory(directory, baseDirectory);
+
+        //    if (format == "csv")
+        //        WriteToCsvFile(jsonContent, directory, baseDirectory, fileName, directory);
+        //    else
+        //    {
+        //        using (var writer = new StreamWriter(Path.Combine(directory, fileName + "." + format), false))
+        //        {
+        //            writer.Write(jsonContent);
+        //        }
+        //    }
+
+        //}
+
+        //public void WriteToCsvFile(string jsonContent, string directory, string baseDirectory, string fileName, string format)
+        //{
+        //    CheckDirectory(directory, baseDirectory);
+
+        //    var dt = JsonConvert.DeserializeObject<DataTable>(jsonContent);
+
+        //    using (var writer = new StreamWriter(Path.Combine(directory, fileName + "." + format), false, Encoding.UTF8))
+        //    {
+        //        using (var csv = new CsvWriter(writer, CultureInfo.CurrentCulture))
+        //        {
+
+        //            foreach (DataColumn column in dt.Columns)
+        //            {
+        //                csv.WriteField(column.ColumnName);
+        //            }
+        //            csv.NextRecord();
+
+        //            foreach (DataRow row in dt.Rows)
+        //            {
+        //                for (var i = 0; i < dt.Columns.Count; i++)
+        //                {
+        //                    csv.WriteField(row[i]);
+        //                }
+        //                csv.NextRecord();
+        //            }
+        //        }
+        //    }
+        //}
+        //private void GetConfiguration(out string format,
+        //                              out string fileName,
+        //                              out string baseUrl,
+        //                              out string directoryName,
+        //                              out TimeSpan interval)
+        //{
+        //    format = ConfigurationManager.AppSettings["format"];
+        //    fileName = ConfigurationManager.AppSettings["filename"];
+        //    directoryName = ConfigurationManager.AppSettings["dirname"];
+        //    baseUrl = ConfigurationManager.AppSettings["url"];
+        //    interval = new TimeSpan(Convert.ToInt64(ConfigurationManager.AppSettings["interval"]));
+        //}
 
         protected override void OnStop()
         {
             _canceled = true;
         }
     }
+
 }
